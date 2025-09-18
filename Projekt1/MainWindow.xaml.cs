b102cfa84cf800c5d39c2e5fe0f59087cfbccc98
User <User@DESKTOP-C410990>
2025-09-18 18:44:01 +0200

    Dodaj pliki projektów.

---
+++ Projekt1/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void Akcja(object sender, RoutedEventArgs e)
+        {
+           string tekst1 = jeden.Text;
+
+           dwa.Text = tekst1;
+        }
+    }
+}
